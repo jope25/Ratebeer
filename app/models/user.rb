1e47841c31083353e497abd917ee6842932f82d3
jope25 <jolmari10@gmail.com>
2017-02-05 23:50:58 +0200

    week 3, 1-5

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  include RatingAverage
+
+  has_secure_password
+
+  validates :username, uniqueness: true,
+            length: { minimum: 3,
+                      maximum: 30 }
+
+  has_many :ratings
+  has_many :beers, through: :ratings
+end
