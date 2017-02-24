0ac192ab7166a8a186f34d90f2fd7ef3100b48f2
jope25 <jolmari10@gmail.com>
2017-02-25 00:49:10 +0200

    heroku problems

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,13 @@
 class SessionsController < ApplicationController
   def new
+    redirect_to current_user if current_user
   end
 
   def create
     user = User.find_by username: params[:username]
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to user_path(user), notice: "Welcome back!"
+      redirect_to user, notice: "Welcome back!"
     else
       redirect_to :back, notice: "Username and password do not match"
     end
